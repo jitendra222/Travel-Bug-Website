2828ceaccceeb6df7ea71948beae121a98b45899
JITENDRA RATHOD <jitendra@techis.io>
2022-06-07 07:17:26 +0530

    new

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function myFunction() {
-    var x = document.getElementById("nav-items");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
